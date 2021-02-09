d77491ca235af94557478dfd093d885425189d88
mibricks <78813866+mibricks@users.noreply.github.com>
2021-02-09 10:22:11 -0700

    Update transport_FH.py
    Added SNL's copyright notice to the main code.

--- transport_FH.py
+++ transport_FH.py
@@ -1,3 +1,8 @@
+'''
+Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
+'''
+
 import numpy as np
 import numpy.linalg as lin
 import scipy.sparse as sps
